1b307dec65b0c66073c3efe7f9d970bc2911f71b
irinaWinter <lobanova.irina.96@mail.ru>
2019-05-28 16:36:30 +0300

    Добавление объекта (а не строки в список пользователей)

--- registration.js
+++ registration.js
@@ -8,16 +8,21 @@ function UserList() {
 	this.type = 'список пользователей';
 	this.users = [];
 	this.add = function(User) {
-		return this.users.push(User.firstName + ' ' + User.lastName + ', ' + User.regDate)
+		// return this.users.push(User.firstName + ' ' + User.lastName + ', ' + User.regDate)
+		return this.users.push(User)
 	};
 	this.getAllUsers = function() {
 		return this.users;
 	}
 }
 
+
+
 function registration() {
 	var name = prompt('Введите имя и фамилию', '');
+	var arr = userList.getAllUsers();
 	if (name !== null && name!== '') {
+
 		var arr = name.split(' ');
 		var firstName = arr[0];
 		var lastName = arr[1];
@@ -53,12 +58,34 @@ function registration() {
 		};
 		var regDate = day() + '.' + month() + '.' + year + ' ' + hours() + ':' + minutes();
 		var myUser = new User(firstName, lastName, regDate);
-		userList.add(myUser);
-		registration();
+
+		// Проверка на наличие регистрации
+		// var matches = 0;
+		// if (arr.length > 0) {
+		// 	for (var i = 0; i < arr.length; i++) {
+		// 		if (firstName == arr[i].firstName && lastName == arr[i].users.lastName) {
+		// 			matches++;
+		// 			console.log(arr[i].firstName);
+		// 		}
+		// 	}
+		// }	
+		// if (matches > 0) {
+		// 	alert('Пользователь с такими данными уже зарегистрирован! Введите другие данные.');
+		// } else {
+		// 	userList.add(myUser);
+		// }
+		// // userList.add(myUser);
+		// // console.log(userList);
+		// // console.log(myUser);
+		// registration();
+		// console.log(userList);
+		
+		
+
 	} else {
-		var arr = userList.getAllUsers();
+		
 		arr.forEach(function(item) {
-			console.log(item);
+			console.log(item.firstName + ' ' + item.lastName + ', ' + item.regDate);
 		});
 	}
 }
