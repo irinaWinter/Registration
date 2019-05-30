969ea106f05b3c3939e740bb2fa7d8f753fba8e0
irinaWinter <lobanova.irina.96@mail.ru>
2019-05-30 16:56:46 +0300

    Проверка на наличие числа.

--- registration.js
+++ registration.js
@@ -16,90 +16,103 @@ function UserList() {
 	}
 }
 
-// Раскомментировать !!!!!
 
-// function registration() {
-// 	var name = prompt('Введите имя и фамилию', '');
-
-
-
-// 	var allUsers = userList.getAllUsers();
-// 	if (name !== null && name!== '') {
-// 		var dataEntered = name.split(' ');
-// 		var firstName = dataEntered[0];
-// 		var lastName = dataEntered[1];
-// 		var date = new Date();
-// 		var day = function() {
-// 			if (date.getDate() < 10) {
-// 				return '0' + date.getDate();
-// 			} else {
-// 				return date.getDate();
-// 			}
-// 		};	
-// 		var month = function() {
-// 			if (date.getMonth() + 1 < 10) {
-// 				return '0' + Number((date.getMonth() + 1), 10);
-// 			} else {
-// 				return date.getMonth() + 1;
-// 			}	
-// 		};
-// 		var year = date.getFullYear();
-// 		var hours = function() {
-// 			if (date.getHours() < 10){
-// 				return '0' + date.getHours();
-// 			} else {
-// 				return date.getHours();
-// 			}	
-// 		};
-// 		var minutes = function() {
-// 			if (date.getMinutes() < 10){
-// 				return '0' + date.getMinutes();
-// 			} else {
-// 				return date.getMinutes();
-// 			}	
-// 		};
-// 		var regDate = day() + '.' + month() + '.' + year + ' ' + hours() + ':' + minutes();
-// 		var myUser = new User(firstName, lastName, regDate);
-
-// 		// Проверка на наличие регистрации
-// 		var matches = 0;
-// 		if (allUsers.length > 0) {
-// 			for (var i = 0; i < allUsers.length; i++) {
-// 				if (firstName == allUsers[i].firstName && lastName == allUsers[i].lastName) {
-// 					matches++;
-// 				}
-// 			}
-// 		}
-
-// 		if (matches > 0) {
-// 			alert('Пользователь с такими данными уже зарегистрирован! Введите другие данные.');
-// 		} else {
-// 			userList.add(myUser);
-// 		}
-// 		registration();
-
-// 	} else {
-// 		allUsers.forEach(function(item) {
-// 			console.log(item.firstName + ' ' + item.lastName + ', ' + item.regDate);
-// 		});
-// 	}
-// }
-
-// var userList = new UserList();
-// registration();
-
-
-function getFullName() {
-	var n = prompt('Введите имя и фамилию', '');
-	var dataEntered = n.split(' ');
-	var firstName = dataEntered[0];
-	var lastName = dataEntered[1];	
-	if (+firstName == false && +lastName == false) {
-		alert('Все верно');
-	} else {
-		alert('Введите данные в корректной форме');
-		getFullName();
+function registration() {
+	function getName() {
+		var name = prompt('Введите имя и фамилию', '');
+		if (name !== null && name !== '') {
+			var dataEntered = name.split(' ');
+			var firstName = dataEntered[0];
+			var lastName = dataEntered[1];
+		}
+		if ( isNaN(+firstName) && isNaN(+lastName)) {
+			return name;
+		} else {
+			alert("Введите корректные данные");
+			getName();
+		}
 	}
+	
+
+
+
+
+
+
+	var allUsers = userList.getAllUsers();
+	// if (name !== null && name !== '') { перенесено в функцию
+		var dataEntered = getName().split(' ');
+		var firstName = dataEntered[0];
+		var lastName = dataEntered[1];
+
+		// Проверка типа данных
+		// if ( isNaN(+firstName) && isNaN(+lastName)) {
+
+		// } else {
+		// 	alert('Введите данные в корректной форме');
+
+		// }
+	
+
+		var date = new Date();
+		var day = function() {
+			if (date.getDate() < 10) {
+				return '0' + date.getDate();
+			} else {
+				return date.getDate();
+			}
+		};	
+		var month = function() {
+			if (date.getMonth() + 1 < 10) {
+				return '0' + Number((date.getMonth() + 1), 10);
+			} else {
+				return date.getMonth() + 1;
+			}	
+		};
+		var year = date.getFullYear();
+		var hours = function() {
+			if (date.getHours() < 10){
+				return '0' + date.getHours();
+			} else {
+				return date.getHours();
+			}	
+		};
+		var minutes = function() {
+			if (date.getMinutes() < 10){
+				return '0' + date.getMinutes();
+			} else {
+				return date.getMinutes();
+			}	
+		};
+		var regDate = day() + '.' + month() + '.' + year + ' ' + hours() + ':' + minutes();
+		var myUser = new User(firstName, lastName, regDate);
+
+		// Проверка на наличие регистрации
+		var matches = 0;
+		if (allUsers.length > 0) {
+			for (var i = 0; i < allUsers.length; i++) {
+				if (firstName == allUsers[i].firstName && lastName == allUsers[i].lastName) {
+					matches++;
+				}
+			}
+		}
+
+		if (matches > 0) {
+			alert('Пользователь с такими данными уже зарегистрирован! Введите другие данные.');
+		} else {
+			userList.add(myUser);
+		}
+		registration();
+
+	// } else { 
+		allUsers.forEach(function(item) {
+			console.log(item.firstName + ' ' + item.lastName + ', ' + item.regDate);
+		});
+	// }
+
 }
 
-getFullName();
+var userList = new UserList();
+registration();
+
+
